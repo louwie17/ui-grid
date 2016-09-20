0b004918bda4aaa7755a6d300de9e5af8fc07d79
Lourens Schep <lourens.schep@viacomcontractor.com>
2016-09-20 19:07:28 -0300

    test(emptyBaseLayer): added tests for empty rows
    Added tests for empty rows, and removed unecessary directive

--- src/features/empty-base-layer/test/emptyBaseLayer.spec.js
+++ src/features/empty-base-layer/test/emptyBaseLayer.spec.js
@@ -1,6 +1,6 @@
 fdescribe('ui.grid.emptyBaseLayer', function () {
 
-  var scope, element, timeout;
+  var scope, element, timeout, viewportHeight, emptyBaseLayerContainer;
 
   beforeEach(module('ui.grid.emptyBaseLayer'));
 
@@ -10,6 +10,7 @@ fdescribe('ui.grid.emptyBaseLayer', function () {
     scope = $rootScope;
     timeout = $timeout;
 
+    viewportHeight = "100";
     scope.gridOptions = {};
     scope.gridOptions.data = [
       { col1: 'col1', col2: 'col2' }
@@ -17,6 +18,10 @@ fdescribe('ui.grid.emptyBaseLayer', function () {
     scope.gridOptions.onRegisterApi = function (gridApi) {
       scope.gridApi = gridApi;
       scope.grid = gridApi.grid;
+      var renderBodyContainer = scope.grid.renderContainers.body;
+      spyOn(renderBodyContainer, 'getViewportHeight').and.callFake(function() {
+        return viewportHeight;
+      });
     };
 
     //$httpBackend.when('GET', 'expandableRowTemplate.html').respond("<div class='test'></div>");
@@ -26,9 +31,23 @@ fdescribe('ui.grid.emptyBaseLayer', function () {
       $compile(element)(scope);
     });
     $timeout.flush();
+
+    emptyBaseLayerContainer = angular.element(element.find('.ui-grid-empty-base-layer-container')[0]);
+    //scope.grid.buildStyles
   }));
 
-  it('emptyBaseLayerContainer should be added to the viewport html', function () {
+  it('should add emptyBaseLayerContainer to the viewport html', function () {
     expect(element.find('.ui-grid-empty-base-layer-container').length).toBe(1);
+  });
+
+  it('should add fake rows to the empty base layer container, on building styles', function() {
+    expect(emptyBaseLayerContainer.children().length).toBe(4);
+  });
+
+  it('should increase in rows if viewport height increased', function() {
+    viewportHeight = "150";
+    scope.grid.buildStyles();
+    scope.$digest();
+    expect(emptyBaseLayerContainer.children().length).toBe(5);
   });
 });
